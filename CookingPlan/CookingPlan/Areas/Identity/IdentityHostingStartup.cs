cbaae7320b10c0a7330c52a0a9ed238026ff0445
Arpedia <planes.next1@gmail.com>
2019-07-03 05:54:54 +0900

    IdentityのViewファイルを生成

---
+++ CookingPlan/CookingPlan/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using CookingPlan.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CookingPlan.Areas.Identity.IdentityHostingStartup))]
+namespace CookingPlan.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
