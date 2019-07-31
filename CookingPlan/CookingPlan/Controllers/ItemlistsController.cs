e00e9c837593496e2b13781db9c1207852b0f924
Arpedia <planes.next1@gmail.com>
2019-07-31 18:57:25 +0900

    Postメソッドで指定の日にちを受け取る

--- CookingPlan/CookingPlan/Controllers/ItemlistsController.cs
+++ CookingPlan/CookingPlan/Controllers/ItemlistsController.cs
@@ -35,5 +35,27 @@ namespace CookingPlan.Controllers
             ViewBag.Date = Others.Const.DateList();
             return View(plans);
         }
+
+        public async Task<IActionResult> Checklist([Bind("Date")] DateTime date)
+        {
+            ViewBag.selected_date = date;
+            /*
+            if (date.Length == 0)
+            {
+                date = DateTime.Today.AddDays(3).ToLongDateString();
+            }
+            */
+            var plans = _context.Plan
+                .Include(p => p.Meal)
+                .Where(p => p.Date >= DateTime.Today && p.Date <= date);
+            
+            foreach (var plan in plans)
+            {
+                var meals = await _context.Meal.Include(m => m.Ingredients).FirstAsync(m => m.Id == plan.MealId);
+                plan.Meal = meals;
+            }
+
+            return View(plans);
+        }
     }
 }
